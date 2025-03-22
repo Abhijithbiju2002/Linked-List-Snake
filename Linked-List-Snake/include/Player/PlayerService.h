43e72e00ec7eec80e2ceedea7f9ff5afea2f3adc
Abhijithbiju2002 <abhiju7@gmail.com>
2025-03-22 16:49:54 +0530

    commit 3(b2)
    setting up position and direction

--- Linked-List-Snake/include/Player/PlayerService.h
+++ Linked-List-Snake/include/Player/PlayerService.h
@@ -3,7 +3,6 @@
 
 namespace Player {
 
-	
 
 	class PlayerService
 	{
--- Linked-List-Snake/include/Player/SnakeController.h
+++ Linked-List-Snake/include/Player/SnakeController.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <SFML/System/Vector2.hpp>
+#include "Player/Direction.h"
 
 namespace Player {
 
@@ -10,7 +12,12 @@ namespace Player {
 	{
 	private:
 		const int initial_snake_lengeth = 10;
+		
+		const sf::Vector2i default_position = sf::Vector2i(25, 13);
+		const Direction default_direction = Direction::RIGHT;
+
 		SnakeState current_snake_state;
+		Direction current_snake_direction;
 
 		void processPlayerInput();
 		void updateSnakeDirection();
